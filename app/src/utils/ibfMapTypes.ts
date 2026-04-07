999d17534bff27be6bed4a306511f9e184ae865f
e3r3i3k3 <ehill@redcross.nl>
2026-04-07 14:46:29 +0200

    PR feedback

--- app/src/utils/ibfMapTypes.ts
+++ app/src/utils/ibfMapTypes.ts
@@ -9,67 +9,65 @@
 //  - Buildings (also MVT) 
 
 
-export interface InfrastructureExposure {
-  // counts of exposed infrastructure, by type
-  // The numbers are [0] exposed, [1] total for affected regions.
-  // Note: if we think this will be more dynamic, we can just change this
-  // to a list of objects that have numbers and string labels.
-  // The front end would just display them in the oder received, and wouldn't
-  // need to know the specific types.
-  
-  shelters: [number, number];
-  roads: [number, number];
-  schools: [number, number];
-  waterPoints: [number, number];
-  clinics: [number, number];
+export interface ExposedItem {
+  unit: string;
+  label: string;
+  key: string;
+  exposed: number;
+  total: number
 }
+  
 
 // Most general information about an event.
-// I don't forsee the payload being that large, but we need to check against real data.
+// I don't foresee the payload being that large, but we need to check against real data.
 // Data layers and more detailed info would be queried through separate API calls
 export interface EventOverviewData {
-  eventType: string; // e.g. flood, drought, etc.
+  // Hazard types for this event in a list
+  // e.g. flood, drought, etc.
+  hazardType: string[];
+
   eventName: string; // Human readable name
   eventId: string;
 
   // For alert level, we'll need to map this on the FE for coloring.
   // The value here is more like an enum val, that we map to a string/color on the FE 
-  alertLevel: string;
+  alertClass: string;
+
+  // Whether this is a triggering event or not.
+  trigger: boolean;
 
   // affects where we zoom to, and where we place the icon on the map
   centroid: [number, number]; // [lon, lat]
 
   // Event time range
-  startDate: string; // ISO date string with hours
-  endDate: string; // ISO date string with hours
+  startTime: string; // ISO date string with hours
+  endTime: string; // ISO date string with hours
 
   // Event creation/update times
-  eventCreatedDate: string; // ISO date string
-  eventLastUpdatedDate: string; // ISO date string
+  firstIssuedAt: string; // ISO date string
+  lastUpdatedAt: string; // ISO date string
 
-  // Lists of details for each affected admin region, grouped by admin level (0, 1, 2...)
+  // Lists of details for each exposed admin region, grouped by admin level (0, 1, 2...)
   // Note: I added admin0 here, although we only went up to admin 1 in the pipelines.
   // Should we just add admin0 to the pipelines? It makes it much easier for the FE 
-  affectedAdminRegions: EventAdminDetail[][];
+  exposedAdminRegions: EventAdminAreaData[][];
 
   // ID for the raster image layer, or null if none
   // TODO: we need more layers. Should this be a list of layers? Or just more vals like this?
   rasterImageId: string | null;
 
-  // sources used for the data (Glofas, etc.), with confidence score 0-100
-  // Question: Or do we want this 0 to 1 range?
-  dataSources: Record<string, number>;
+  // sources used for the data (Glofas, etc.)
+  dataSources: string[];
 
 }
 
 // Each admin area has this info
-export interface EventAdminDetail {
-  adminCode: string;
+export interface EventAdminAreaData {
+  placeCode: string;
   adminLevel: number;
-  adminName: string;
-  impactedPopulation: number;
-  impactedHouseholds: number;
-  infrastructureExposure: InfrastructureExposure;
+  name: string;
+  exposedPopulation: number;
+  exposedInfrastructure: ExposedItem[];
 }
 
 // This would be a list of all upcoming events
@@ -85,9 +83,9 @@ export type AllEventsData = Record<string, EventOverviewData>;
 export interface CountryMapData {
   // ids of layers we have for this country
   // i.e. population, water_points, etc.
-  avaialableLayers: string[];
+  availableLayers: string[];
 
-  // We need some way to to know if IBF supports this or not,
+  // We need some way to know if IBF supports this or not,
   // and if so what kind of support (IBF, MRW, etc.)
   nrwSupport: string;
 }
