ba7f1d80cb9cf03f25960a760881b045ec267b2b
Niels Bom <nbom@redcross.nl>
2026-06-15 16:47:48 +0200

    chore: get lint-css passing, focus only on NRW CSS files

--- app/stylelint.config.js
+++ app/stylelint.config.js
@@ -1,14 +1,11 @@
-import fs from 'fs';
 import path from 'path';
 
 const __filename = new URL(import.meta.url).pathname;
 const __dirname = path.dirname(__filename);
 
-const uiCssPath = path.resolve(__dirname, './node_modules/@ifrc-go/ui/dist/index.css');
-
 const cssPaths = [
     path.resolve(__dirname, './src/index.css'),
-    ...(fs.existsSync(uiCssPath) ? [uiCssPath] : []),
+    path.resolve(__dirname, './node_modules/@ifrc-go/ui/dist/index.css'),
     path.resolve(__dirname, './src/components/NrwMap/OlDataMap/styles.module.css'),
 ];
 
