94875d5f89b8494cafc9d657a5f986ff9702667e
e3r3i3k3 <ehill@redcross.nl>
2026-07-17 09:58:53 +0200

    update map

--- app/src/utils/nrw/nrwConstants.ts
+++ app/src/utils/nrw/nrwConstants.ts
@@ -6,4 +6,4 @@ export const defaultMapZoom = 3;
 // Mapbox style
 // This is the Map created in Mapbox Studio with the format `mapbox://styles/<username>/<style_id>`
 // Get this from Mapbox Studio > Styles > Style url
-export const NRW_MAPBOX_STYLE_URL = 'mapbox://styles/510global/cmrdb6i91000a01qrf8j675uc';
+export const NRW_MAPBOX_STYLE_URL = 'mapbox://styles/510global/cmrls7huy001501sde6mdhzlk';
