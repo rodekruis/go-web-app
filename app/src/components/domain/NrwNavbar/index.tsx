caa136f2bcf2fa6455bacbc3f1c3077978586dc2
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2026-07-27 11:13:08 +0200

    feat: remove country label from navbar

--- app/src/components/domain/NrwNavbar/index.tsx
+++ app/src/components/domain/NrwNavbar/index.tsx
@@ -1,70 +1,39 @@
-import {
-    faBars,
-    faCircleUser,
-} from '@fortawesome/pro-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-    Heading,
-    ListView,
-    PageContainer,
-} from '@ifrc-go/ui';
-import { useTranslation } from '@ifrc-go/ui/hooks';
-import {
-    _cs,
-    isTruthyString,
-} from '@togglecorp/fujs';
+import { faBars, faCircleUser } from "@fortawesome/pro-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Heading, ListView, PageContainer } from "@ifrc-go/ui";
+import { useTranslation } from "@ifrc-go/ui/hooks";
+import { _cs } from "@togglecorp/fujs";
 
-import i18n from './i18n.json';
-import styles from './styles.module.css';
+import i18n from "./i18n.json";
+import styles from "./styles.module.css";
 
 interface Props {
-    className?: string;
-    countryName?: string;
+  className?: string;
 }
 
 function NrwNavbar(props: Props) {
-    const {
-        className,
-        countryName,
-    } = props;
+  const { className } = props;
 
-    const strings = useTranslation(i18n);
+  const strings = useTranslation(i18n);
 
-    return (
-        <nav className={_cs(styles.navbar, className)}>
-            <PageContainer
-                className={styles.pageContainer}
-                contentClassName={styles.content}
-            >
-                <ListView
-                    withWrap
-                    withSpaceBetweenContents
-                >
-                    <div className={styles.brand}>
-                        <FontAwesomeIcon
-                            icon={faBars}
-                            className={styles.icon}
-                        />
-                        <Heading
-                            level={3}
-                            className={styles.heading}
-                        >
-                            {strings.nrwNavbarTitle}
-                            {isTruthyString(countryName) && (
-                                <span className={styles.countryName}>
-                                    {countryName}
-                                </span>
-                            )}
-                        </Heading>
-                    </div>
-                    <FontAwesomeIcon
-                        icon={faCircleUser}
-                        className={styles.iconUser}
-                    />
-                </ListView>
-            </PageContainer>
-        </nav>
-    );
+  return (
+    <nav className={_cs(styles.navbar, className)}>
+      <PageContainer
+        className={styles.pageContainer}
+        contentClassName={styles.content}
+      >
+        <ListView withWrap withSpaceBetweenContents>
+          <div className={styles.brand}>
+            <FontAwesomeIcon icon={faBars} className={styles.icon} />
+            <Heading level={3} className={styles.heading}>
+              {strings.nrwNavbarTitle}
+            </Heading>
+          </div>
+          <FontAwesomeIcon icon={faCircleUser} className={styles.iconUser} />
+        </ListView>
+      </PageContainer>
+    </nav>
+  );
 }
 
 export default NrwNavbar;
--- app/src/views/CountryProfileNationalRiskWatch/index.tsx
+++ app/src/views/CountryProfileNationalRiskWatch/index.tsx
@@ -1,20 +1,13 @@
-import { useMemo } from 'react';
-import { useSearchParams } from 'react-router-dom';
 import {
     Container,
     ListView,
 } from '@ifrc-go/ui';
 import { useTranslation } from '@ifrc-go/ui/hooks';
-import {
-    isTruthyString,
-    unique,
-} from '@togglecorp/fujs';
 
 import NrwMap from '#components/domain/NrwMap';
 import NrwNavbar from '#components/domain/NrwNavbar';
 import Page from '#components/Page';
 import { nrwStandalone } from '#config';
-import useCountry from '#hooks/domain/useCountry';
 
 import i18n from './i18n.json';
 import styles from './styles.module.css';
@@ -22,35 +15,6 @@ import styles from './styles.module.css';
 // eslint-disable-next-line import/prefer-default-export
 export function Component() {
     const strings = useTranslation(i18n);
-
-    const [searchParams] = useSearchParams();
-    const allCountries = useCountry();
-
-    const countryName = useMemo(
-        () => {
-            // ?countryCodeIso3=ETH,MWI or ?countryCodeIso3=ETH&countryCodeIso3=MWI
-            const countryCodes = searchParams
-                .getAll('countryCodeIso3')
-                .flatMap((value) => value.split(','))
-                .map((code) => code.trim().toUpperCase())
-                .filter(isTruthyString);
-
-            const uniqueCountryCodes = unique(countryCodes);
-
-            // only show a country name when exactly one country is selected
-            if (uniqueCountryCodes.length !== 1) {
-                return undefined;
-            }
-
-            const [countryCode] = uniqueCountryCodes;
-            const matchedCountry = allCountries?.find(
-                (country) => country.iso3.toUpperCase() === countryCode,
-            );
-
-            return matchedCountry?.name;
-        },
-        [searchParams, allCountries],
-    );
 
     const content = (
         <Container
@@ -68,7 +32,7 @@ export function Component() {
     if (nrwStandalone) {
         return (
             <>
-                <NrwNavbar countryName={countryName} />
+                <NrwNavbar />
                 <Page
                     title={strings.nationalRiskWatchPageTitle}
                     className={styles.page}
