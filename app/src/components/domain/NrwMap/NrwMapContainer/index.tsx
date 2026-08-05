fe6d714228f7f007b7a38167593b10ad77ab45e3
Niels Bom <nbom@redcross.nl>
2026-08-05 16:43:43 +0200

    refactor: split NrwMap

---
+++ app/src/components/domain/NrwMap/NrwMapContainer/index.tsx
@@ -0,0 +1,91 @@
+import 'mapbox-gl-v3/dist/mapbox-gl.css';
+
+import {
+    useEffect,
+    useRef,
+} from 'react';
+import { _cs } from '@togglecorp/fujs';
+import mapboxgl from 'mapbox-gl-v3';
+
+import {
+    mbtoken,
+    nrwStandalone,
+} from '#config';
+import type NrwLngLat from '#views/CountryProfileNationalRiskWatch/NrwLngLat';
+import {
+    type Latitude,
+    type Longitude,
+    type MapViewChangeHandler,
+    type Zoom,
+} from '#views/CountryProfileNationalRiskWatch/types';
+
+import styles from './styles.module.css';
+
+// This component wraps Mapbox so the rest of the components don't need to know
+// about Mapbox.
+// This component does not know about URLs or network requests.
+
+// Get this from Mapbox Studio > Styles > Style url
+const NRW_MAPBOX_STYLE_URL = 'mapbox://styles/510global/cmrls7huy001501sde6mdhzlk';
+
+function NrwMapContainer(props: {
+    zoom: Zoom;
+    center: NrwLngLat;
+    onMapViewChange: MapViewChangeHandler;
+}) {
+    const {
+        zoom,
+        center,
+        onMapViewChange,
+    } = props;
+
+    // The element inside of which the map will be rendered.
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    // Initialize the Mapbox map instance
+    useEffect(() => {
+        if (!containerRef.current) {
+            return undefined;
+        }
+
+        mapboxgl.accessToken = mbtoken;
+
+        const map = new mapboxgl.Map({
+            container: containerRef.current,
+            style: NRW_MAPBOX_STYLE_URL,
+            projection: 'mercator',
+            attributionControl: true,
+            center,
+            zoom,
+        });
+
+        map.addControl(new mapboxgl.NavigationControl({ showCompass: false }));
+
+        map.on('moveend', () => {
+            onMapViewChange(
+                map.getZoom() as Zoom,
+                map.getCenter().lat as Latitude,
+                map.getCenter().lng as Longitude,
+            );
+        });
+
+        // Cleanup.
+        return () => {
+            map?.remove();
+        };
+    // Set the dependencies to empty since we want this to run exactly once on mount.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return (
+        <div
+            ref={containerRef}
+            className={_cs(
+                styles.nrwMapContainer,
+                nrwStandalone && styles.nrwStandalone,
+            )}
+        />
+    );
+}
+
+export default NrwMapContainer;
--- app/src/components/domain/NrwMap/index.tsx
+++ app/src/components/domain/NrwMap/index.tsx
@@ -1,30 +1,14 @@
-import 'mapbox-gl-v3/dist/mapbox-gl.css';
-
-import {
-    useEffect,
-    useRef,
-} from 'react';
-import { _cs } from '@togglecorp/fujs';
-import mapboxgl from 'mapbox-gl-v3';
-
-import {
-    mbtoken,
-    nrwStandalone,
-} from '#config';
 import type NrwLngLat from '#views/CountryProfileNationalRiskWatch/NrwLngLat';
 import {
-    type Latitude,
-    type Longitude,
     type MapViewChangeHandler,
     type Zoom,
 } from '#views/CountryProfileNationalRiskWatch/types';
 
-import styles from './styles.module.css';
+import NrwMapContainer from './NrwMapContainer';
 
-// Get this from Mapbox Studio > Styles > Style url
-const NRW_MAPBOX_STYLE_URL = 'mapbox://styles/510global/cmrls7huy001501sde6mdhzlk';
+// This component knows nothing about Mapbox.
 
-export default function NrwMap(props: {
+function NrwMap(props: {
     zoom: Zoom;
     center: NrwLngLat;
     onMapViewChange: MapViewChangeHandler;
@@ -35,51 +19,13 @@ export default function NrwMap(props: {
         onMapViewChange,
     } = props;
 
-    // The element inside of which the map will be rendered.
-    const containerRef = useRef<HTMLDivElement>(null);
-
-    // Initialize the Mapbox map instance
-    useEffect(() => {
-        if (!containerRef.current) {
-            return undefined;
-        }
-
-        mapboxgl.accessToken = mbtoken;
-
-        const map = new mapboxgl.Map({
-            container: containerRef.current,
-            style: NRW_MAPBOX_STYLE_URL,
-            projection: 'mercator',
-            attributionControl: true,
-            center,
-            zoom,
-        });
-
-        map.addControl(new mapboxgl.NavigationControl({ showCompass: false }));
-
-        map.on('moveend', () => {
-            onMapViewChange(
-                map.getZoom() as Zoom,
-                map.getCenter().lat as Latitude,
-                map.getCenter().lng as Longitude,
-            );
-        });
-
-        // Cleanup.
-        return () => {
-            map?.remove();
-        };
-    // Set the dependencies to empty since we want this to run exactly once on mount.
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
     return (
-        <div
-            ref={containerRef}
-            className={_cs(
-                styles.nrwMap,
-                nrwStandalone && styles.nrwStandalone,
-            )}
+        <NrwMapContainer
+            zoom={zoom}
+            center={center}
+            onMapViewChange={onMapViewChange}
         />
     );
 }
+
+export default NrwMap;
