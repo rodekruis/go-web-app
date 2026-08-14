cb85abe8f3da19ad30f8dccf4973f51507c6a8d0
Niels Bom <nbom@redcross.nl>
2026-08-14 19:30:05 +0200

    feat: add useNrwEvents hook

---
+++ app/src/hooks/domain/useNrwEvents.ts
@@ -0,0 +1,48 @@
+import {
+    useCallback,
+    useMemo,
+} from 'react';
+import { isFalsyString } from '@togglecorp/fujs';
+
+import {
+    type NrwApiUrlQuery,
+    useNrwRequest,
+} from '#utils/restRequest';
+
+function useNrwEvents(props: NrwApiUrlQuery<'/events'>) {
+    const {
+        countryCodeIso3,
+        active = true,
+        timestamp,
+    } = props;
+
+    const query = useMemo(
+        () => ({
+            countryCodeIso3,
+            active,
+            timestamp,
+        }),
+        [countryCodeIso3, active, timestamp],
+    );
+
+    const response = useNrwRequest({
+        url: '/events',
+        apiType: 'nrw',
+        query,
+        skip: isFalsyString(countryCodeIso3),
+    });
+
+    const refetch = useCallback(
+        () => {
+            response.retrigger();
+        },
+        [response],
+    );
+
+    return {
+        ...response,
+        refetch,
+    };
+}
+
+export default useNrwEvents;
