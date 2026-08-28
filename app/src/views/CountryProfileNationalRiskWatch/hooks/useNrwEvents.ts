9b261e22fc7ff415c2b588496174476ba3a85f85
e3r3i3k3 <ehill@redcross.nl>
2026-08-28 08:35:58 +0200

    pr feedback

--- app/src/views/CountryProfileNationalRiskWatch/hooks/useNrwEvents.ts
+++ app/src/views/CountryProfileNationalRiskWatch/hooks/useNrwEvents.ts
@@ -1,14 +1,11 @@
+import { useMemo } from 'react';
+
 import { useNrwRequest } from '#utils/restRequest';
 
 import { type CountryCodeIso3 } from '../types';
 
 function useNrwEvents(countries: CountryCodeIso3[]) {
-    // For one country: pass the country code to the query to fetch data for only there.
-    // For more than one country: pass no country to fetch all events, and then filter the results.
-    // Making multi-country requests as a request for all countries is generally faster than
-    // chaining calls to the backend API. The NRW API may be later made to support
-    // multi-country requests, but for now this is the best approach.
-    const singleCountry = countries.length === 1 ? countries[0] : undefined;
+    const isSingleCountry = countries.length === 1;
 
     const {
         response,
@@ -17,13 +14,20 @@ function useNrwEvents(countries: CountryCodeIso3[]) {
     } = useNrwRequest({
         url: '/events',
         apiType: 'nrw',
-        query: {
-            countryCodeIso3: singleCountry,
-        },
+        // For one country: pass the country code to the query to fetch data for only there.
+        // For more than one country: pass no country to fetch all events and filter the results.
+        ...(isSingleCountry && {
+            query: { countryCodeIso3: countries[0] },
+        }),
     });
 
-    const events = response?.filter(
-        (event) => countries.some((country) => country === event.countryCodeIso3),
+    // Use memo here to prevent spamming dependencies with a new `events` reference.
+    // This would be hit every map pan/zoom.
+    const events = useMemo(
+        () => response?.filter(
+            (event) => countries.some((country) => country === event.countryCodeIso3),
+        ),
+        [response, countries],
     );
 
     return {
