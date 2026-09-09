7e03774fa08f26fb822e4a67beee412f76e7279d
e3r3i3k3 <ehill@redcross.nl>
2026-09-09 11:39:02 +0200

    undone

--- app/src/components/domain/NrwMap/index.tsx
+++ app/src/components/domain/NrwMap/index.tsx
@@ -73,15 +73,8 @@ function NrwMap(props: {
                 return;
             }
 
-            const populationAvailable = availableLayers.some(
-                ({ name, type }) => name === 'population' && type === 'raster',
-            );
-
-            if (!populationAvailable) {
-                return;
-            }
-
-            countries.forEach((countryCodeIso3) => loadLayer(countryCodeIso3, 'population'));
+            // eslint-disable-next-line no-console
+            console.log('Available layers for countries', countries, availableLayers);
         },
 
         [availableLayers, countries, countriesResolved, loadLayer],
