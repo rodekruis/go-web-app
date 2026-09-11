4289ca191492d0c75bacfbbd4420c348d79aa5bc
e3r3i3k3 <ehill@redcross.nl>
2026-09-11 15:25:43 +0200

    Set layers allowed

--- app/src/components/domain/NrwMap/NrwLayerPanel/index.tsx
+++ app/src/components/domain/NrwMap/NrwLayerPanel/index.tsx
@@ -4,10 +4,18 @@ import {
 } from '@fortawesome/pro-regular-svg-icons';
 import { faSquareCheck } from '@fortawesome/pro-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useTranslation } from '@ifrc-go/ui/hooks';
 
 import { type NrwLayer } from '#views/CountryProfileNationalRiskWatch/types';
 
+import i18n from './i18n.json';
 import styles from './styles.module.css';
+
+// Layers supported in the panel, in display order.
+const supportedLayerNames = [
+    'population',
+    'floodDepth',
+] as const satisfies readonly NrwLayer['name'][];
 
 function NrwLayerPanel(props: {
     layers: NrwLayer[] | undefined;
@@ -16,7 +24,13 @@ function NrwLayerPanel(props: {
 }) {
     const { layers, isLayerVisible, onToggleLayer } = props;
 
-    const hasLayers = (layers?.length ?? 0) > 0;
+    const strings = useTranslation(i18n);
+
+    const supportedLayers = supportedLayerNames
+        .map((name) => layers?.find((layer) => layer.name === name))
+        .filter((layer) => layer !== undefined);
+
+    const hasLayers = supportedLayers.length > 0;
 
     return (
         <div className={styles.layerPanel}>
@@ -25,11 +39,11 @@ function NrwLayerPanel(props: {
                     icon={faLayerGroup}
                     className={styles.titleIcon}
                 />
-                Layers
+                {strings.nrwLayerPanelTitle}
             </div>
             <div className={styles.items}>
-                {!hasLayers && 'No layers available'}
-                {layers?.map((layer) => {
+                {!hasLayers && strings.nrwLayerPanelNoLayersMessage}
+                {supportedLayers.map((layer) => {
                     const isVisible = isLayerVisible(layer.name);
 
                     return (
